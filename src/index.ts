2c153f6f5ed36d0dd738508a17547d374792b0ab
Michalis Michopoulos <michopoulos@192.168.1.84>
2023-11-09 19:07:07 +0200

    Initialize rest api scaffold

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as dotenv from "dotenv";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+/**
+ * App Variables
+ */
+
+if (!process.env.PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
+
+/**
+ *  App Configuration
+ */
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+/**
+ * Server Activation
+ */
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+const router = express.Router();
+
+app.use("/", router);
+
+router.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello ktelon");
+});
