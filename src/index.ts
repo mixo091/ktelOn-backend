e455929a7b62d76a302cebdefbd3e70c41e9bb53
thanoskaravangelis <thanosblv@gmail.com>
2023-11-18 18:25:53 +0100

    Create endpoints to get lines based on island

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import * as dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import helmet from "helmet";
-import { trips } from "./data/trips";
+import { paros_lines } from "./data/paros_schedule";
+//import { trips } from "./data/trips";
 
 dotenv.config();
 
@@ -42,7 +43,48 @@ router.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello ktelon");
 });
 
-
+/*
 router.get("/trips" , (req:Request , res:Response) => {
     res.status(200).json(trips);
 })
+*/
+
+router.get("/:island/lines", (req: Request, res: Response) => {
+  const island = req.params.island;
+  if (island === "paros") {
+    res.status(200).json(paros_lines);
+  } else {
+    res.status(404).send("Island not found");
+  }
+});
+
+router.get("/:island/lines/:id", (req: Request, res: Response) => {
+  const island = req.params.island;
+  const id = parseInt(req.params.id);
+  const line = paros_lines.find((line) => line.id === id);
+  if (line) {
+    res.status(200).json(line);
+  } else {
+    res.status(404).send("Line not found");
+  }
+});
+
+// get all to stations from a specific station
+router.get(
+  "/:island/lines/:id/from/:station_id",
+  (req: Request, res: Response) => {
+    const island = req.params.island;
+    const id = parseInt(req.params.id);
+    const station_id = parseInt(req.params.station_id);
+    const line = paros_lines.find((line) => line.id === id);
+    if (line) {
+      // get stations after specific station depending on order array
+      const toStations = line.stations.filter((station) => {
+        return line.order.indexOf(station.id) > line.order.indexOf(station_id);
+      });
+      res.status(200).json(toStations);
+    } else {
+      res.status(404).send("Line not found");
+    }
+  }
+);
